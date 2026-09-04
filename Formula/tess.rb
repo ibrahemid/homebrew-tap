bed7cbcf1f7c4690de22ba8d7029896ce16feabb
ibrahemid <ibrahemid@gmail.com>
2026-09-05 00:36:03 +0800

    tess 1.1.0

--- Formula/tess.rb
+++ Formula/tess.rb
@@ -1,7 +1,7 @@
 class Tess < Formula
   desc "CLI-first TOTP/2FA authenticator for macOS"
   homepage "https://tessera.ibrahemid.com/"
-  version "1.0.2"
+  version "1.1.0"
   license "Apache-2.0"
 
   livecheck do
@@ -12,22 +12,22 @@ class Tess < Formula
   on_macos do
     on_arm do
       url "https://github.com/ibrahemid/tessera/releases/download/v#{version}/tess_#{version}_darwin_arm64.tar.gz"
-      sha256 "f0b41aa4a75baf57dd6f55e9191e3dcac4761653f4fa2ca0ced85001810be18d"
+      sha256 "75df08b715dc31a326d25061a64171423bce245cf6f5943753577470a0275568"
     end
     on_intel do
       url "https://github.com/ibrahemid/tessera/releases/download/v#{version}/tess_#{version}_darwin_amd64.tar.gz"
-      sha256 "16c16f0adb8bd967a4597b0d86cc7b6d2cafa2995c1539b64e757a69fc34a491"
+      sha256 "84cd31b461cc868cdb219b3da47c7885fff70f9a6b501a69258d1b5aea5399f7"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ibrahemid/tessera/releases/download/v#{version}/tess_#{version}_linux_arm64.tar.gz"
-      sha256 "1988dcf8dcca0eafd3ae8d9b16dd978db799ff06464a841a46f2386df2f115fe"
+      sha256 "baf83665ca822a8f75882cc9e08178593d8ca65bbf8c6deb3d4158fbb4264346"
     end
     on_intel do
       url "https://github.com/ibrahemid/tessera/releases/download/v#{version}/tess_#{version}_linux_amd64.tar.gz"
-      sha256 "3758e770c7da4547fb4e31e54fd97e5c9761468d4a356fe41ee3170ac52f838f"
+      sha256 "94144967eddcc5635ce2f1087ea51644fc7195b8482a7c28c12622804b933387"
     end
   end
 
